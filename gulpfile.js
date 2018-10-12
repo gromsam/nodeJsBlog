1a6963124375a2375026f7d27c72f4c4103cea00
gromsam <gromsam@mail.ru>
2018-10-12 22:59:06 +0300

    commit_18_10_12_22_58

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulpfile = require('gulp'),
+    nodemon = require('nodemon'),
+    uglify = require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
+    concat = require('gulp-concat'); // Подключаем gulp-concat (для конкатенации файлов)
+
+gulpfile.task('def', function() {
+    console.log('Hello Gulp!')
+});
+
+gulpfile.task('scripts', function() {
+    return gulpfile.src([
+        'views/bower_components/jquery/dist/jquery.js',
+        'views/bower_components/magnific-popup/dist/jquery.magnific-popup.js',
+        'views/js/cookie.jqery.js',
+        'views/js/move-top.js',
+        '/js/easing.js'
+    ]).pipe(concat('libs.min.js')).pipe(uglify()).pipe(gulpfile.dest('views/js'));
+});
+
+
+gulpfile.task('server', ['scripts'], function() {
+    nodemon({
+        script: 'app.js',
+        ext: 'js'
+    }).on('restart', () => {
+        gulp.src('app.js').pipe(notify('Running the start tasks and stuff'));
+    });
+});
