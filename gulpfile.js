f257be6a04ef095456fb659d619241bd8870a2c5
gromsam <gromsam@mail.ru>
2018-10-13 22:22:18 +0300

    commit_18_10_13_22_22

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,29 @@
 var gulpfile = require('gulp'),
+    sass = require('gulp-sass'),
+    cssnano  = require('gulp-cssnano'),
+    rename  = require('gulp-rename'),
     nodemon = require('nodemon'),
     uglify = require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
-    concat = require('gulp-concat'); // Подключаем gulp-concat (для конкатенации файлов)
+    concat = require('gulp-concat'),
+    autoprefixer = require('gulp-autoprefixer');
 
 gulpfile.task('def', function() {
     console.log('Hello Gulp!')
+});
+
+gulpfile.task('sass', function(){ // Создаем таск Sass
+    return gulpfile.src('app/sass/**/*.sass') // Берем источник
+        .pipe(sass())
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
+        .pipe(gulpfile.dest('views/css')) // Выгружаем результата в папку app/css
+        // .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+});
+
+gulpfile.task('css-libs', ['sass'], function() {
+    return gulpfile.src('views/css/libs.css') // Выбираем файл для минификации
+        .pipe(cssnano()) // Сжимаем
+        .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
+        .pipe(gulpfile.dest('views/css')); // Выгружаем в папку app/css
 });
 
 gulpfile.task('scripts', function() {
@@ -17,12 +36,17 @@ gulpfile.task('scripts', function() {
     ]).pipe(concat('libs.min.js')).pipe(uglify()).pipe(gulpfile.dest('views/js'));
 });
 
+// gulpfile.task('css', function() {
+//     return gulpfile.src([
+//         'views/bower_components/bootstrap/dist/bootstrap.css'
+//     ]).pipe(cssnano()).pipe(gulpfile.dest('views/css'));
+// });
 
-gulpfile.task('server', ['scripts'], function() {
+gulpfile.task('dev', ['css-libs','scripts'], function() {
     nodemon({
         script: 'app.js',
         ext: 'js'
     }).on('restart', () => {
-        gulp.src('app.js').pipe(notify('Running the start tasks and stuff'));
+        gulpfile.src('app.js').pipe(notify('Running the start tasks and stuff'));
     });
 });
